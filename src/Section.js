58f6aaaeeb36f14091d400518638ec31ebd136b1
Zachary Golba <zachary.golba@postlight.com>
2020-05-06 18:11:55 -0400

    feat: save values in search params
    feat: save values in search params
    
    feat: save values in search params

--- src/Section.js
+++ src/Section.js
@@ -1,81 +1,89 @@
-import 'util';
-import numeral from 'numeral';
-import React, {useState, useEffect} from 'react';
-import Slider from './Slider';
-import Statement from './Statement';
-import Text from './Text';
+import "util";
+import numeral from "numeral";
+import useSearchParams from "@postlight/use-search-params";
+import React from "react";
+import Slider from "./Slider";
+import Statement from "./Statement";
+import Text from "./Text";
 
-import './Section.css';
+import "./Section.css";
 
 function Section(props) {
-    const [state, setState] = useState(props.astState);
-    const [ast, setAST] = useState(props.ast);
+  const { ast, astState } = props;
+  const searchParams = useSearchParams("replace");
 
-    useEffect(() => {
-	setState(props.astState);
-	setAST(props.ast);	
-    }, [props.astState, props.ast]);
-    
-    function addField(k,v) {
-	setState({...state, [k]:v});
-	return v;
+  function addField(k, v) {
+    searchParams.set(k, v);
+    return v;
+  }
+
+  function readField(k) {
+    return searchParams.get(k) || props.astState[k];
+  }
+
+  function readFields() {
+    return Object.fromEntries(
+      Object.entries(astState).map(([name, defaultValue]) => {
+        const value = +searchParams.get(name);
+        return [name, Number.isNaN(value) ? defaultValue : value];
+      })
+    );
+  }
+
+  function toComponents(o, i) {
+    switch (o.type) {
+      case "text":
+        return <Text key={i} {...o} />;
+
+      case "statement":
+        return (
+          <span className="full-statement" key={i}>
+            <Slider
+              key={o.variable}
+              addField={addField}
+              valueFromState={readField(o.variable)}
+              i={i}
+              {...o}
+            />
+            <Statement valueFromState={readField(o.variable)} i={i} {...o} />
+          </span>
+        );
+
+      case "expression":
+        function f(n) {
+          const num = numeral(n);
+          if (n < 10 && n > -10) {
+            return num.format("0.00");
+          }
+          return num.format("-0,0");
+        }
+
+        function calc(o) {
+          const state = readFields();
+          const result = o.eval(state);
+          if (result !== state[o.variable] && !isNaN(result)) {
+            addField(o.variable, result);
+          }
+          return result;
+        }
+
+        return (
+          <span className="expression" key={i}>
+            {f(calc(o))}
+          </span>
+        );
+
+      default:
+        return undefined;
     }
-    
-    function toComponents(o, i) {
-	switch (o.type) {
-	    
-	case 'text':
-	    return <Text key={i} {...o}/>
-	    
-	case 'statement':
-	    return <span className="full-statement" key={i}>
-		       <Slider
-			   key={o.variable}
-			   addField={addField}
-			   valueFromState={state[o.variable]}
-			   i={i}
-			   {...o}
-		       />
-		       <Statement
-			   valueFromState={state[o.variable]}
-			   i={i}
-			   {...o}
-		       />
-		       
-		   </span>
-	    
-	case 'expression':
-	    function f(n) {
-		const num = numeral(n);
-		if (n < 10 && n > -10) {
-		    return num.format('0.00');
-		}
-		return num.format('-0,0');
-	    }
+  }
 
-	    function calc(o) {
-		const result = o.eval(state);
-		if (result!==state[o.variable] && !isNaN(result)) {
-		    addField(o.variable, result);
-		}
-		return result;
-	    }
-	    
-	    return <span className="expression" key={i}>
-		       {f(calc(o))}
-		   </span>;
-	    
-	default:
-	    return undefined;
-	}
-	    
-    }
-
-    return <div id="text">
-	       <h1 key='h1'>{props.page}</h1>
-	       {ast.map(toComponents)}
-	   </div>;
-    
+  return (
+    <div id="text">
+      <h1 key="h1">{props.page}</h1>
+      {ast.map(toComponents)}
+    </div>
+  );
 }
 
 export default Section;
