88d6ac5e890d4ee89b0ff6675de8efe41bf4d649
Paul Ford <ford@ftrain.com>
2020-05-15 17:16:50 -0400

    bug: fix hamburger

--- src/LineChart.js
+++ src/LineChart.js
@@ -6,14 +6,18 @@ function LineChart(props) {
     <ResponsiveLine
       data={props.data}
       yScale={{
-        type: "linear",
-        min: props.minValue,
-        max: props.maxValue,
+            type: "linear",
+            min: props.minValue,
+            max: props.maxValue,
+	    
       }}
       animate={false}
       margin={{ top: 0, right: 10, bottom: 45, left: 70 }}
       xScale={{ type: "point" }}
-      curve="monotoneX"
+	curve="monotoneX"
+            enableArea={true}
+            areaOpacity={0.07}
+	
       axisBottom={{
         orient: "bottom",
         tickSize: 5,
