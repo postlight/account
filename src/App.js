d7295cab29d1b43693e7bf1d34cfb907a4953fc5
John Holdun <john@johnholdun.com>
2022-11-14 15:33:09 -0800

    Support embeds

--- src/App.js
+++ src/App.js
@@ -14,14 +14,32 @@ const textVars = Object.fromEntries(
 );
 
 function App() {
-  let { page } = useParams();
-  if (!textVars[page]) return <Navigate to="/soda" />;
+  const { page } = useParams();
+  const embedded = new URLSearchParams(window.location.search).has('embed');
+
+  if (!textVars[page]) {
+    return <Navigate to="/soda" />;
+  }
+
   const [ast, astState, rawText] = textVars[page];
 
   return (
-    <div className="App">
-      <Nav textVars={textVars} />
-      <Section key={page} ast={ast} astState={astState} rawText={rawText} page={page} />
+    <div className={`App ${embedded ? 'App--embedded' : ''}`}>
+      {embedded ? null : <Nav textVars={textVars} />}
+      <Section key={page} ast={ast} astState={astState} rawText={rawText} page={page} embedded={embedded} />
+      <footer>
+        <span>
+          {embedded ? <>
+            This is{' '}
+            <a href="https://account.postlight.com" target="_blank" rel="noreferrer">Account</a>, a
+          </> : 'A'}
+          {' '}
+          Labs project from your friends at
+        </span>
+        <a href="https://postlight.com/careers" {...(embedded ? { target: '_blank', re: 'noreferrer' } : {})}>
+          <img alt="Postlight, an NTT DATA Company" src="/postlight-logo.svg" width={embedded ? 63 : 125} height={embedded ? 16 : 32} />
+        </a>
+      </footer>
     </div>
   );
 }
